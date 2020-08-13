f581313a84eed37108eb71e85bc43e6e2e3de596
Uwe Jettkant <uj@macbook-pro.betahaushh.lan>
2020-08-13 15:50:28 +0200

    fixed buck
    Signed-off-by: Uwe Jettkant <uj@macbook-pro.betahaushh.lan>

--- src/js/hamburgerMenue.js
+++ src/js/hamburgerMenue.js
@@ -1,1 +1,11 @@
-console.log('logic will be placed here')
+const closeButton = document.querySelector('.close__nav')
+const openButton = document.querySelector('.open__nav')
+const nav = document.querySelector('.navigation')
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('navigation__open')
+})
+
+openButton.addEventListener('click', () => {
+  nav.classList.add('navigation__open')
+})
